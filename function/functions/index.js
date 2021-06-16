7f9dc321ff5a3cfd091fbcd4d249aabebe7b98b4
JoaquinHoffmann <joaquinhoffmannb@gmail.com>
2021-06-16 15:48:07 -0400

    commit on '3.-Notificaciones'

--- function/functions/index.js
+++ function/functions/index.js
@@ -1,9 +1,23 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:/Users/joaqu/Documents/Flutter/flutter_fcm/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+exports.sendPushNotification = functions.firestore.document('/places/{notificationId}').onCreate((snap, context) => {
+    var values = snap.data();
+    var token = values.fcmToken;     
+    
+    var payload = {
+      notification: {
+        title: "Tutulo correspondiente", //values.title,
+        body: "Cuerpo correspondiente"//values.message
+        }    
+    }   
+    
+    return admin.messaging().sendToDevice(token, payload);
+  });
